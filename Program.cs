3d93400f5d617f66953075051d074e17cc73da46
KirillSkachedubov <k.skachedubov@carx-tech.com>
2024-11-14 17:43:26 +0300

    2.1 - Типы переменных в коде

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            bool b00l = false;
+            byte b = 1;
+            short s = 0;
+            long l = 0;
+            float f = 2.3f;
+            double d = 1.2;
+
+            char c = 's';
+            string str = "Hello";
+        }
+    }
+}
